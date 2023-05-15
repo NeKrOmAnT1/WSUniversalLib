e2b3bfc3b18565bd7ef6a49e61c1852ada3c6731
stud <stud@107-1>
2023-05-15 11:27:59 +0400

    Добавьте файлы проекта.

---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        /// <summary>
+        /// Метод рассчитывает целое количество сырья, необходимого для производства определенного количества продукции, учитывая возможный брак материалов.
+        /// Для упрощения расчетов продукция будет считаться прямооугольной формы
+        /// </summary>
+        /// <param name="productType"> тип продукции </param>
+        /// <param name="materialType">тип материла</param>
+        /// <param name="count"> количество продукции </param>
+        /// <param name="width"> ширина  </param>
+        /// <param name="length"> длина </param>
+        /// <returns> Если все данные верны, то возращается количетво необходимого сырья(округленную в большую сторону).Если неверные параметры, то возвращается -1 </returns>
+
+         
+        public int GetQuantityForProduct(
+            int productType, int materialType,
+            int count, float width, float length
+            )
+        {
+
+            if (count <=0 || width <=0 || length <=0 || productType <=0 || materialType <=0)
+                return -1;
+            
+            float coef = 0;
+            float per = 0;
+            switch (productType)
+            {
+                case 1:
+                    coef = 1.1f;
+                    break;
+                case 2:
+                    coef = 2.5f;
+                    break ;
+                case 3:
+                    coef = 8.43f;
+                    break;
+                
+            }
+            switch (materialType)
+            {
+                case 1:
+                    per = 0.003f;
+                    break;
+                case 2:
+                    per = 0.0012f;
+                    break;
+            }
+
+            var countproduct = width * length * count * coef;
+            countproduct = (int)Math.Ceiling(countproduct);
+            float cont = (countproduct * per) + countproduct;
+            return (int)Math.Ceiling(cont);
+        }
+
+    }
+}
---
+++ WSUniversalLibTests/CalculationTests.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSUniversalLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace WSUniversalLib.Tests
+{
+    [TestClass()]
+    public class CalculationTests
+    {
+        [TestMethod()]
+        public void GetQuantityForProductTest_114148()
+        {
+            //подготовка
+            Calculation calculation1= new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = 15;
+            float width = 20f;
+            float length = 45f;
+
+            int expected = 114148;
+            //тест
+            var actual = calculation1.GetQuantityForProduct(typeProduct, typeMaterial, count, width, length);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest_bad_tp()
+        {
+            //подготовка
+            Calculation calculation1= new Calculation();
+            int typeProduct = -3;
+            int typeMaterial = 1;
+            int count = 15;
+            float width = 20f;
+            float length = 45f;
+
+            int expected = -1;
+            //тест
+            var actual = calculation1.GetQuantityForProduct(typeProduct, typeMaterial, count, width, length);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest_bad_tm()
+        {
+            //подготовка
+            Calculation calculation1 = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = -4;
+            int count = 15;
+            float width = 20f;
+            float length = 45f;
+
+            int expected = -1;
+            //тест
+            var actual = calculation1.GetQuantityForProduct(typeProduct, typeMaterial, count, width, length);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest_bad_c()
+        {
+            //подготовка
+            Calculation calculation1 = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = -29;
+            float width = 20f;
+            float length = 45f;
+
+            int expected = -1;
+            //тест
+            var actual = calculation1.GetQuantityForProduct(typeProduct, typeMaterial, count, width, length);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest_bad_w()
+        {
+            //подготовка
+            Calculation calculation1 = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = 15;
+            float width = -20f;
+            float length = 45f;
+
+            int expected = -1;
+            //тест
+            var actual = calculation1.GetQuantityForProduct(typeProduct, typeMaterial, count, width, length);
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void GetQuantityForProductTest_bad_l()
+        {
+            //подготовка
+            Calculation calculation1 = new Calculation();
+            int typeProduct = 3;
+            int typeMaterial = 1;
+            int count = 15;
+            float width = 20f;
+            float length = -45f;
+
+            int expected = -1;
+            //тест
+            var actual = calculation1.GetQuantityForProduct(typeProduct, typeMaterial, count, width, length);
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
